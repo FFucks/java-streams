6ffb4d12eeb3ea4a7bc0bf7116b304b5e3cc60fb
FFucks <fabio_geiss@hotmail.com>
2025-06-09 22:10:50 -0300

    bingo stream

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,63 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        StreamClass streamClass = new StreamClass();
+        List<String> bingoPool = new ArrayList<>(75);
+        int start = 1;
+        for (char c : "BINGO".toCharArray()) {
+            for (int i = start; i < (start + 15); i ++) {
+                bingoPool.add("" + c + i);
+                System.out.println("" + c + i);
+            }
+            start += 15;
+        }
+        System.out.println("------------------------------");
+
+        Collections.shuffle(bingoPool);
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+
+        System.out.println("------------------------------");
+
+        //List<String> firstOnes = bingoPool.subList(0, 15);
+        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0, 15));
+        firstOnes.sort(Comparator.naturalOrder());
+        firstOnes.replaceAll(s -> {
+            if (s.indexOf('G') == 0 || s.indexOf('O') == 0) {
+                String updated = s.charAt(0) + "-" + s.substring(1);
+                System.out.println(updated + " ");
+                return updated;
+            }
+            return s;
+        });
+
+        System.out.println("\n------------------------------");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+
+        System.out.println("\n------------------------------");
+
+        bingoPool.stream()
+                .limit(15)
+                .filter(s -> s.indexOf('G') == 0 || s.indexOf('O') == 0)
+                .map(s -> s.charAt(0) + "-" + s.substring(1))
+                .sorted()
+                .forEach(s -> System.out.println(s + " "));
+
+        System.out.println("\n------------------------------");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+
+
+        /*StreamClass streamClass = new StreamClass();
         List<String> listString = Arrays.asList("teste", "str", "lalala");
 
         //Empty Stream
@@ -23,6 +73,6 @@ public class Application {
 
         //Iterate
         Stream<Integer> streamIterated = Stream.iterate(40, n -> n + 2).limit(20);
-
+        */
     }
 }
